b1eb0dddebafb09bf56870ef919f274264682cbf
Abel Magana <abelmagana88920@gmail.com>
2022-09-22 16:33:31 +0800

    Alennahs First filr

---
+++ LoopHelloWorld/src/loophelloworld/LoopHelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loophelloworld;
+
+/**
+ *
+ * @author Abel Magana
+ */
+public class LoopHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5;
+        while (x > 0) {
+            System.out.println("Hello World");
+            x--;
+        }
+    }
+    
+}
